a76354b808d56a36391b7da6a6687f81c5b1db45
stephensedition <stephen@seditionart.com>
2019-07-24 17:49:34 +0100

    Create names.rb

---
+++ names.rb
@@ -0,0 +1,12 @@
+# Game of Thrones characters
+name1 = "Joffrey Baratheon"
+name2 = "Arya Stark"
+name3 = "Beric Dondarrion"
+name4 = "Melisandre"
+
+# Your code goes here
+
+puts name1;
+puts name2;
+puts name3;
+puts name4;
