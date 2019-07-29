b5a923e0f1eba0d11402d4fc34419d3d92f219ba
stephensedition <stephen@seditionart.com>
2019-07-29 11:47:15 +0100

    add two files about queen lyrics

---
+++ queen.rb
@@ -0,0 +1,18 @@
+# A little bit of classic rock
+
+lyrics = "Is this the real life?"\
+         "Is this just fantasy?"\
+         "Caught in a landslide,"\
+         "No escape from reality."
+
+# Your code goes here
+
+lyrics = [
+	"Is this the real life?", 	# 0 
+	"Is this just fantasy?", 	# 1
+	"Caught in a landslide", 	# 2
+	"No escape from reality."	# 3
+]
+
+puts lyrics
+
